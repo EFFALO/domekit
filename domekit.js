d398da76d52b15d7c3d91a7169c438af7b27d0eb
robby <robbykraft@gmail.com>
2011-10-22 18:42:10 -0700

    revert a commit

--- domekit.js
+++ domekit.js
@@ -421,41 +421,16 @@ domekit.Controller.prototype.strutLengths = function() {
   var j;
   var k;
   var found;
-  var connections = [];
-  connections = this.visibleConnections_;
-  console.log("CONNECTIONS: ",this.visibleConnections_.length);
-  /*var neighbors = [];
-  for(i=0;i<this.points_.length;i++){
-    neighbors = this.findNeighbors(i);
-    for(j=0;j<neighbors.length;j++) connections.push([i,neighbors[j]]);
-  }
-  connections = this.connections_;
-  // remove duplicates
-  for(i=0;i<connections.length;i++){
-    neighbors = connections[i];
-    for(j=i;j<connections.length;j++){
-      if(neighbors[0] == connections[j][1] && neighbors[1] == connections[j][0]) connections.splice(j,1);
-    }
-  }*/
-  for(i=0;i<connections.length; i++) console.log(connections[i][0], connections[i][1]);
-  for(i = 0; i < connections.length; i++){
-    distance = Math.sqrt( ((this.points_[connections[i][0]].x-this.points_[connections[i][1]].x)*(this.points_[connections[i][0]].x-this.points_[connections[i][1]].x)) +
-                          ((this.points_[connections[i][0]].y-this.points_[connections[i][1]].y)*(this.points_[connections[i][0]].y-this.points_[connections[i][1]].y)) +
-                          ((this.points_[connections[i][0]].z-this.points_[connections[i][1]].z)*(this.points_[connections[i][0]].z-this.points_[connections[i][1]].z)) );
+  for(i = 0; i < this.connections_.length; i++){
+    distance = Math.sqrt( ((this.points_[this.connections_[i][0]].x-this.points_[this.connections_[i][1]].x)*(this.points_[this.connections_[i][0]].x-this.points_[this.connections_[i][1]].x)) +
+                          ((this.points_[this.connections_[i][0]].y-this.points_[this.connections_[i][1]].y)*(this.points_[this.connections_[i][0]].y-this.points_[this.connections_[i][1]].y)) +
+                          ((this.points_[this.connections_[i][0]].z-this.points_[this.connections_[i][1]].z)*(this.points_[this.connections_[i][0]].z-this.points_[this.connections_[i][1]].z)) );
     distance+=nudger;
-    found = -1;
+    found = 0;
     for(k = 0; k < struts.length; k++){
-      if(Math.floor(struts[k]*100000000.0) == Math.floor(distance*100000000.0)) found = k;
+      if(Math.floor(struts[k]*100000000.0) == Math.floor(distance*100000000.0)) found = 1;
     }
-    if(found == -1) {
-      struts.push(distance);  //push new strut
-      strutCount[struts.length-1] = 1;  //begin counting struts
-      console.log("creating strut ",struts.length-1);
-    }
-    else {
-      strutCount[found]++;
-      console.log("add 1 to ",found);
-    }
+    if(found == 0) struts.push(distance);
   }   
   return struts;
   //END OF FUNCTION  - PREVIEW RESULTS BELOW
